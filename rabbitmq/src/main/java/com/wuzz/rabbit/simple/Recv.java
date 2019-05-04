5ead8f60ac53cb715fee1fbfbf5cc0a2e3851bba
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-05-04 20:50:26 +0800

    简单消息消费者新的写法

--- rabbitmq/src/main/java/com/wuzz/rabbit/simple/Recv.java
+++ rabbitmq/src/main/java/com/wuzz/rabbit/simple/Recv.java
@@ -1,6 +1,7 @@
 package com.wuzz.rabbit.simple;
 
 import com.rabbitmq.client.*;
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory;
 import com.wuzz.rabbit.ConnectionUtils;
 
 import java.io.IOException;
@@ -8,11 +9,41 @@ import java.util.concurrent.TimeoutException;
 
 public class Recv {
     private static final String QUEUE_NAME = "test_simple_queue";
+
     /**
-     * 消费者
+     * 消费者 (新方法)
      * @param args
      */
     public static void main(String args[]) throws IOException,TimeoutException,ShutdownSignalException,ConsumerCancelledException,InterruptedException
+
+    {
+        // 先拿到连接
+        Connection connection = ConnectionUtils.getConnection();
+        // 获取一个通道
+        Channel channel = connection.createChannel();
+        // 常见队列声明
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        // 事件触发的回调函数
+
+        DefaultConsumer consumer =  new DefaultConsumer(channel){
+            // 一旦有消息进入队列，则触发此方法
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String msgStr = new String(body,"utf-8");
+                System.out.println("new RECV " + msgStr);
+            }
+        };
+        //  监听队列
+        channel.basicConsume(QUEUE_NAME,true,consumer);
+
+
+    }
+
+    /**
+     * 消费者 (老方法)
+     * @param args
+     */
+    public static void oldmain(String args[]) throws IOException,TimeoutException,ShutdownSignalException,ConsumerCancelledException,InterruptedException
 
     {
         // 先拿到连接
