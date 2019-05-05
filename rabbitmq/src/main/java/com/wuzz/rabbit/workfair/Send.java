f6d58f871f942d40295fb0d181c41049dea93750
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-05-05 23:10:42 +0800

    队列和消息的持久化

--- rabbitmq/src/main/java/com/wuzz/rabbit/workfair/Send.java
+++ rabbitmq/src/main/java/com/wuzz/rabbit/workfair/Send.java
@@ -2,6 +2,7 @@ package com.wuzz.rabbit.workfair;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.MessageProperties;
 import com.wuzz.rabbit.ConnectionUtils;
 
 import java.io.IOException;
@@ -21,8 +22,8 @@ public class Send {
         Connection connection = ConnectionUtils.getConnection();
         // 获取一个通道
         Channel channel = connection.createChannel();
-        // 常见队列声明
-        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        // 常见队列声明 （队列持久化）
+        channel.queueDeclare(QUEUE_NAME,true,false,false,null);
 
         //每次回执之前只发送一条
         channel.basicQos(1);
@@ -30,8 +31,8 @@ public class Send {
         //发送50条消息
         for (int i = 0; i < 50 ; i++) {
             String msg = "hello " + i;
-
-            channel.basicPublish("",QUEUE_NAME,null,msg.getBytes());
+            // 消息持久化
+            channel.basicPublish("",QUEUE_NAME,MessageProperties.PERSISTENT_TEXT_PLAIN,msg.getBytes());
 
             System.out.println("---send msg-" + msg);
 
