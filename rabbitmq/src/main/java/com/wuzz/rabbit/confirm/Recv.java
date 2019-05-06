ebb374f0906ee495faefe50fee32f4c716f307ce
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-05-07 00:44:58 +0800

    confirm 确认机制异步回调

--- rabbitmq/src/main/java/com/wuzz/rabbit/confirm/Recv.java
+++ rabbitmq/src/main/java/com/wuzz/rabbit/confirm/Recv.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeoutException;
 
 public class Recv {
 
-        private static final String QUEUE_NAME = "test_queue_confirm1";
+        private static final String QUEUE_NAME = "test_queue_confirm3";
 
         public static void main (String args[]) throws IOException, TimeoutException {
 
